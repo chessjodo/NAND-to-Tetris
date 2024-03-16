1d8aa0a5576587d4c9432cfa26d5ee119008ebbb
Jonas Dornieden <jonas.dornieden@student.auc.nl>
2024-03-16 23:13:17 +0100

    Added DOCSTRINGS to explain every function and cleaned up debugging statements

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -69,6 +69,7 @@ LABEL_DICT = {}
 DESTKEYS = [key for key in DEST.keys() if key != None]
 JUMPKEYS = [key for key in JUMP.keys() if key != None]
 COMPKEYS = []
+# Insert escape characters if special characters are used in COMP key
 for key in COMP.keys():
     if matched := re.search("(\||\+|\-)", key):
         new_key = key[: matched.start()] + "\\" + key[matched.start() :]
@@ -79,13 +80,21 @@ LABELKEYS = []
 
 
 def return_value(text):
+    """
+    return_value(text): Takes a string (text) which starts with a '@' sign and
+    translates it to a 16 digit binary code which is returned.
+    To do this the characters after the @ are checked.
+    If they are integers, the integer is returned in binary.
+    If it is the name of a label elsewhere in the code,
+    the line number is taken from the label dictionary.
+    If it is an R0-R15 code or another predifined symbol,
+    the values are also taken from the respective dictionary.
+    If the symbol does not yet exist, it is defined and added to the dictionary.
+    """
     try:
         return f"0{int(text[1:-2]):015b}"
     except:
-        print("TEXT: ", text)
         if matched := re.search("({})XX".format("|".join(LABELKEYS)), text):
-            # print("PrintLABEL DICT", LABEL_DICT, matched.groups())
-            # print("RegEx Labelkeys: ", "({})".format("|".join(LABELKEYS)))
             return f"0{int(LABEL_DICT[matched.group(1)]):015b}"
         else:
             if matched := re.search("R(1[0-5]|[0-9])XX", text):
@@ -97,11 +106,16 @@ def return_value(text):
                     return f"0{int(SYMBOLS[matched.group(1)]):015b}"
                 else:
                     SYMBOLS[text[:-2]] = f"{len(SYMBOLS)+9}"
-                    print(text + ":: New Symbol")
                     return f"0{int(SYMBOLS[text[:-2]]):015b}"
 
 
 def update_labelkeys():
+    """
+    update_labelkeys(): Goes through the keys of the LABEL_DICT dictionary and
+    adds any necessary escape characters to the keys.
+    The altered keys are added to an array which is used to create
+    the regular expression to look for labels.
+    """
     for key in LABEL_DICT.keys():
         new_key = ""
         for char in key:
@@ -109,19 +123,25 @@ def update_labelkeys():
                 new_key += "\\" + char
             else:
                 new_key += char
-            # print(new_key)
         LABELKEYS.append(new_key)
-    # print(LABELKEYS)
 
 
 def translate_instruction(line):
+    """
+    translate_instruction(line): takes a string (line) and sees if it is of
+    the form    DEST=COMP;JUMP
+    which is the setup of any instruction.
+    The values for the respective parts are retrieved from the respective
+    dictionaries and combined into a single 16-bit binary string
+    that is returned.
+    If the line does not match the format False is returned.
+    """
     if matched := re.match(
         "(({})=)?({})(;({}))?XX".format(
             "|".join(DESTKEYS), "|".join(COMPKEYS), "|".join(JUMPKEYS)
         ),
         line,
     ):
-        # print("GROUPS: ", line, matched.groups())
         dest = DEST[matched.group(2)]
         comp = COMP[matched.group(3)]
         jump = JUMP[matched.group(5)]
@@ -132,30 +152,45 @@ def translate_instruction(line):
 
 def assembler():
     """
-    DOCSTRING
-
+    assembler(): Function that coordinates the translation from assembly to
+    binary codes. It reads an input file and writes to an output file.
+    For every line in the input it matches it to a value using the return_value
+    function, and if that is not successful an instruction using the
+    translate_instruction function. The returned values from these functions is
+    written to the output file.
     """
     output = open("translation.hack", "w")
     input_file = open("input.txt", "r")
-    line_count = -1
     while line := input_file.readline():
-        line_count += 1
         line = line.replace(" ", "")
-        # print("COMPKEYS: ", COMPKEYS)
         if matched := re.match("@[^\s^X^ ]*XX", line):
-            # print("XXX" + line[matched.start() : matched.end()] + "XXX")
             translation = return_value(line[matched.start() : matched.end()])
-            # print("TRANSLATION: ", translation)
             output.writelines(translation + "\n")
         elif combination := translate_instruction(line):
             output.writelines(combination + "\n")
         else:
-            print("No Output: ", line, ord(line[-4]), "Char -4", line[-4])
+            print("The entered text is not in the asked format")
     output.close()
-    print(LABEL_DICT)
+    input_file.close()
 
 
 def add_labels():
+    """
+    add_labels(): Reads input from the standarn input and
+    goes through every line removing the following:
+    - full line comments
+    - in-line comments
+    - spaces before the command
+    - spaces after the command
+
+    If the line contains a label definition of the form (<labelname>), this
+    is added to the LABEL_DICT witht the line number as the value.
+    These lines are then removed.
+    The two characters XX are added to the end of every remaining line
+    containing a command to later know where the instruction ends.
+    These lines are written to the input file, so that it can be traversed more
+    than once later on.
+    """
     input_file = open("input.txt", "w")
     line_count = -1
     while line := stdin.readline():
@@ -168,18 +203,33 @@ def add_labels():
             line = line[matched.end() :]
         if matched := re.search("\s*//.*", line):
             line = line[: matched.start()]
-        # if matched := re.search("\s*", line):
-        #   line = line[: matched.start()] + line[matched.end()]
         if matched := re.search("\((.*)\)", line):
             LABEL_DICT[matched.group(1)] = line_count
             line_count -= 1
             continue
         else:
             input_file.writelines(line + "XX\n")
+    input_file.close()
+
+
+def full_assembly():
+    """
+    full_assembly():
+    First calls the add_labels to traverse the entire input
+    and remove any spaces and comments to only remain with the lines that
+    need to be translated to binary.
+
+    Second the function update_labelkeys is called to make sure that labelkeys
+    are available with included escape characters
+    to be added to regular expressions.
+
+    Lastly, the assembler function is called to do the translation of the input.
+    """
+    add_labels()
+    update_labelkeys()
+    assembler()
 
 
 if __name__ == "__main__":
     print("Running Assembler")
-    add_labels()
-    update_labelkeys()
-    assembler()
+    full_assembly()
